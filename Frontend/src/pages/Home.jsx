0f55af0632959a87a2ae4b28d24dbbb94a7c98a6
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-10 20:17:49 +0530

    feat: update Home component to include Uber logo and improve layout

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <img className="h-16 w-24 p-4" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber logo" />
+      </div>
+      <div> 
+        
+      </div>
+    </div>
   )
 }
 
