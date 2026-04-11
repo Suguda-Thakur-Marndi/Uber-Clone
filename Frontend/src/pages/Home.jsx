df3d4de97f1ad7fedfcc9df8e711b3f4a1ea4c4a
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-11 08:21:52 +0530

    fix: remove unnecessary whitespace in Home component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <img className="h-16 w-24 p-4" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber logo" />
       </div>
       <div> 
-                                    
+      
       </div>
     </div>
   )
