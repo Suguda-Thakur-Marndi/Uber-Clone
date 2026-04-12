43f40414512fc58a2c64e9c1305c1b7ac75a11e6
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-12 13:11:19 +0530

    feat: implement state management for pickup and destination inputs in Home component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,25 +1,47 @@
+import React from "react"
+import { useState } from "react"
 const Home = () => {
+  const [pickup, setpickup] = useState('')
+  const [destination, setdestination] = useState('')
+  const [panel, setpanel] = useState('')
+  const Submithandel=(e)=>{
+    e.preventDefault();
+
+  }
   return (
     <div className="h-screen relative overflow-hidden">
       <div className="fixed top-0 left-0 right-0 z-10 bg-white shadow-sm">
         <img className="h-16 w-24 p-4" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber logo" />
       </div>
       <img className="w-full h-full object-cover" src="https://i.sstatic.net/gtiI7.gif" alt="" role="presentation" /> 
-      <div className=" flex flex-col justify-end absolute h-screen top-0 w-full px-0 py-8 rounded-t-3xl shadow-lg">
+      <div className=" flex flex-col justify-end absolute h-screen top-0 w-full px-0  rounded-t-3xl shadow-lg">
         <div className="h-[30%] p-5 w-full bg-white">
           <h4 className="text-3xl font-bold mb-6 text-gray-900">
             Find Trip
           </h4>
-          <form className="space-y-4">
+          <form className="space-y-4 relative" onSubmit={(e)=>{
+            Submithandel(e)
+
+          }}>
+            <div className="absolute left-7 py-3.5 top-0 h-full rounded-full w-1 flex-col bg-black"></div>
             <input 
-              className="bg-gray-100 px-4 py-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-black transition" 
+
+              className="bg-gray-100 px-4 py-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-black transition pl-12" 
               type="text" 
-              placeholder="📍 Add Pickup location" 
+              placeholder="📍 Add Pickup location"
+              name={pickup}onChange={(e)=>{
+                setpickup(e.target.value)
+              }}
             />
             <input 
-              className="bg-gray-100 px-4 py-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-black transition" 
+              className="bg-gray-100 px-4 py-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-black transition pl-12" 
               type="text" 
-              placeholder="🎯 Enter your destination" 
+              placeholder="🎯 Enter your destination"
+              name={destination}
+               onChange={(e)=>{
+                setdestination(e.target.value)
+
+              }}
             />
           </form>
         </div>
