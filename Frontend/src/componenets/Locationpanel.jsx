65513f0de81ab6c32b51873a5ecb408ddedefdd7
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-15 08:52:44 +0530

    feat: implement Locationpanel component with layout and content

--- Frontend/src/componenets/Locationpanel.jsx
+++ Frontend/src/componenets/Locationpanel.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Locationpanel = () => {
   return (
-    <div>Locationpanel</div>
+    <div>
+       <div className='flex items-center justify-start'>
+         <h2 className='bg-[#eee] p-4 rounded-4xl'><i class="ri-map-pin-line"></i></h2>
+        <h4>
+    Lorem ipsum dolor sit amet.
+        </h4>
+       </div>
+
+    </div>
   )
 }
 
