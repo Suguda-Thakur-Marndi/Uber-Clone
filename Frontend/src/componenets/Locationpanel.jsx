9bab3993b3a570e066e5593e0764a55e4fab97c0
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-16 09:10:04 +0530

    fix: adjust padding and layout in Locationpanel component

--- Frontend/src/componenets/Locationpanel.jsx
+++ Frontend/src/componenets/Locationpanel.jsx
@@ -4,8 +4,8 @@ const Locationpanel = () => {
   return (
     <div>
        <div className='flex items-center justify-start'>
-         <h2 className='bg-[#eee] p-4 rounded-4xl'><i class="ri-map-pin-line"></i></h2>
-        <h4>
+         <h2 className='bg-[#eee] h-10 w-10 items-center flex justify-center rounded-4xl'><i class="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>
     Lorem ipsum dolor sit amet.
         </h4>
        </div>
