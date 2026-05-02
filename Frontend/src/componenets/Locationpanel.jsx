ebd651f4bcc945da2e5d5a12f9bedfcad09108b1
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-05-02 22:38:38 +0530

    fix: add top padding to Locationpanel for improved layout

--- Frontend/src/componenets/Locationpanel.jsx
+++ Frontend/src/componenets/Locationpanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Locationpanel = () => {
   return (
-    <div className='flex flex-col gap-10'>
+    <div className='flex pt-10 flex-col gap-10'>
         <div className='flex items-center gap-4 justify-start'>
          <h2 className='bg-[#eee] h-10 w-10 items-center flex justify-center rounded-4xl'><i className="ri-map-pin-line"></i></h2>
         <h4 className='font-medium'>
