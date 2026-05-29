27e9deca1a97fc04e5029b104563fc17ba1dd046
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-05-29 20:49:22 +0530

    Update Driverride component to increase footer height for better visibility

--- Frontend/src/pages/Driverride.jsx
+++ Frontend/src/pages/Driverride.jsx
@@ -13,7 +13,7 @@ const CaptainRide = () => {
     >
        
     </div>
-    <div className="fixed h-10 bg-amber-50 bottom-0 left-0 right-0 z-10  shadow-sm">
+    <div className="fixed h-60 bg-amber-50 bottom-0 left-0 right-0 z-10  shadow-sm">
 
     </div>
     </div>
